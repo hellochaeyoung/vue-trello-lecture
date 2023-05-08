78580270548904fc805af2c447f8a772dedb9d2e
hellochaeyoung <cyahn0831@hansung.ac.kr>
2023-05-08 23:18:49 +0900

    라우터 링크 사용 및 링크 컴포넌트화

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
   <div id="app">
-    여기서부터 코드를 시작합니다!
+<!--    <a href="/">Home</a>
+    <a href="/login">Login</a>-->
+<!--    <router-link to="/">Home</router-link>
+    <router-link to="/login">Login</router-link>-->
+    <Navbar />
     <router-view></router-view> <!-- 라우팅에 따라 컴포넌트가 바뀌는 부분 -->
   </div>
 </template>
 
 <script>
+import Navbar from "./components/Navbar";
+
 export default {
   name: 'app',
+  components: { Navbar },
   data () {
     return {
     }
